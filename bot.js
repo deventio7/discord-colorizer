993d0f9ca17cd2760ea392614bd6c31a074f2063
deventio <deventio7@gmail.com>
2017-02-04 11:56:27 -0500

    version 0.0.2 - fixed ytdl dependency, added adm commands

--- bot.js
+++ bot.js
@@ -25,6 +25,20 @@ bot.on('ready', () => {
 var errors = '';
 let queue = {};
 
+const admCommands = {
+  'leave': (msg) => {
+    msg.guild.channels.filter(n => {return n.type === 'voice'}).array().forEach(n => {n.leave()});
+    msg.channel.sendMessage(`Leaving all voice channels on the server!`);
+  },
+  'join': (msg) => {
+    return new Promise((resolve, reject) => {
+      const voiceChannel = msg.member.voiceChannel;
+      if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('I couldn\'t connect to your voice channel...');
+      voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
+    });
+  }
+};
+
 const commands = {
   'clear': (msg) => {
     var authorRoles = msg.guild.member(msg.author).roles;
@@ -48,10 +62,10 @@ const commands = {
       sentMsgPromise.then(sent => {sent.delete(3000); msg.delete(3000);}).catch(sent => {sent.delete(); console.log('Failed delivery of message ' + sent.content);});
     } else if (msg.guild.member(bot.user).roles.has(paramRole.id)) {
       var roleId = paramRole.id;
-      roleName = paramRole.name;  
+      roleName = paramRole.name;
       var intersectingColorRoles = authorRoles.keyArray().filter(n => {return (msg.guild.member(bot.user).roles.keyArray().indexOf(n) != -1) && (msg.guild.roles.get(n).color)});
       if (intersectingColorRoles.length > 0 && paramRole.color) {
-        var setRolePromise = msg.guild.member(msg.author).setRoles(authorRoles.filter(n => {return intersectingColorRoles.indexOf(n.id) == -1}).concat(msg.guild.roles.filter(n => {return n.id == roleId})));
+        var setRolePromise = msg.guild.member(msg.author).setRoles(authorRoles.filter(n => {return intersectingColorRoles.indexOf(n.id) == -1}).concat(msg.guild.roles.filter(n => {return n  .id == roleId})));
         setRolePromise.then(() => {
           var sentMsgPromise = msg.channel.sendMessage('Replaced your color role(s) with ' + roleName + ', ' + msg.author.username + '!');
           sentMsgPromise.then(sent => {sent.delete(3000); msg.delete(3000);}).catch(sent => {sent.delete(); console.log('Failed delivery of message ' + sent.content);});
@@ -76,36 +90,36 @@ const commands = {
       sentMsgPromise.then(sent => {sent.delete(3000); msg.delete(3000);}).catch(sent => {sent.delete(); console.log('Failed delivery of message ' + sent.content);});
     }
   },
-	'play': (msg) => {
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with .add!`);
-		if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
-		if (queue[msg.guild.id].playing) return msg.channel.sendMessage('Already Playing!');
-		let dispatcher;
-		queue[msg.guild.id].playing = true;
-		(function play(song) {
-			if (song === undefined) return msg.channel.sendMessage('Queue is empty - add more with `.add <url>`!').then(() => {
-				queue[msg.guild.id].playing = false;
-				//msg.member.voiceChannel.leave();
-			});
-			msg.channel.sendMessage(`Playing: **${song.title}** as requested by: **${song.requester}**`);
-			dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : music_quality });
-			let collector = msg.channel.createCollector(m => m);
-			collector.on('message', m => {
-				if (m.content.toLowerCase().startsWith('.pause')) {
-					msg.channel.sendMessage('Playback paused!').then(() => {dispatcher.pause();});
-				} else if (m.content.toLowerCase().startsWith('.resume')){
-					msg.channel.sendMessage('Playback resumed!').then(() => {dispatcher.resume();});
-				} else if (m.content.toLowerCase().startsWith('.skip')){
-					msg.channel.sendMessage('Current track skipped!').then(() => {dispatcher.end();});
-				} else if (m.content.toLowerCase().startsWith('.volume+')){
-					if (Math.round(dispatcher.volume*50) >= 100) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-					dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
-					msg.channel.sendMessage(`Volume changed to ${Math.round(dispatcher.volume*50)}%!`);
-				} else if (m.content.toLowerCase().startsWith('.volume-')){
-					if (Math.round(dispatcher.volume*50) <= 0) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
-					dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
-					msg.channel.sendMessage(`Volume changed to ${Math.round(dispatcher.volume*50)}%!`);
-				} else if (m.content.toLowerCase().startsWith('.volume')){
+  'play': (msg) => {
+    if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with .add!`);
+    if (!msg.guild.voiceConnection) return commands.join(msg).then(() => commands.play(msg));
+    if (queue[msg.guild.id].playing) return msg.channel.sendMessage('Already Playing!');
+    let dispatcher;
+    queue[msg.guild.id].playing = true;
+    (function play(song) {
+      if (song === undefined) return msg.channel.sendMessage('Queue is empty - add more with `.add <url>`!').then(() => {
+        queue[msg.guild.id].playing = false;
+        //msg.member.voiceChannel.leave();
+      });
+      msg.channel.sendMessage(`Playing: **${song.title}** as requested by: **${song.requester}**`);
+      dispatcher = msg.guild.voiceConnection.playStream(yt(song.url, { audioonly: true }), { passes : music_quality });
+      let collector = msg.channel.createCollector(m => m);
+      collector.on('message', m => {
+        if (m.content.toLowerCase().startsWith('.pause')) {
+          msg.channel.sendMessage('Playback paused!').then(() => {dispatcher.pause();});
+        } else if (m.content.toLowerCase().startsWith('.resume')){
+          msg.channel.sendMessage('Playback resumed!').then(() => {dispatcher.resume();});
+        } else if (m.content.toLowerCase().startsWith('.skip')){
+          msg.channel.sendMessage('Current track skipped!').then(() => {dispatcher.end();});
+        } else if (m.content.toLowerCase().startsWith('.volume+')){
+          if (Math.round(dispatcher.volume*50) >= 100) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+          dispatcher.setVolume(Math.min((dispatcher.volume*50 + (2*(m.content.split('+').length-1)))/50,2));
+          msg.channel.sendMessage(`Volume changed to ${Math.round(dispatcher.volume*50)}%!`);
+        } else if (m.content.toLowerCase().startsWith('.volume-')){
+          if (Math.round(dispatcher.volume*50) <= 0) return msg.channel.sendMessage(`Volume: ${Math.round(dispatcher.volume*50)}%`);
+          dispatcher.setVolume(Math.max((dispatcher.volume*50 - (2*(m.content.split('-').length-1)))/50,0));
+          msg.channel.sendMessage(`Volume changed to ${Math.round(dispatcher.volume*50)}%!`);
+        } else if (m.content.toLowerCase().startsWith('.volume')){
           var param = m.content.split(' ')[1];
           if (param%1 === 0) {
             dispatcher.setVolume(Math.max(Math.min(param,100),0)/50);
@@ -113,35 +127,24 @@ const commands = {
           } else {
             msg.channel.sendMessage(`Please enter a whole number between 0 and 100 inclusive; Current volume is ${Math.round(dispatcher.volume*50)}%.`);
           }
-				} else if (m.content.toLowerCase().startsWith('.time')){
-					msg.channel.sendMessage(`Time in track: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
-				}
-			});
-			dispatcher.on('end', () => {
-				collector.stop();
-				queue[msg.guild.id].songs.shift();
-				play(queue[msg.guild.id].songs[0]);
-			});
-			dispatcher.on('error', (err) => {
-				return msg.channel.sendMessage('Error: ' + err).then(() => {
-					collector.stop();
-					queue[msg.guild.id].songs.shift();
-					play(queue[msg.guild.id].songs[0]);
-				});
-			});
-		})(queue[msg.guild.id].songs[0]);
-	},
-  'leave': (msg) => {
-    msg.guild.channels.filter(n => {return n.type === 'voice'}).array().forEach(n => {n.leave()});
-    msg.channel.sendMessage(`Leaving all voice channels on the server!`);
+        } else if (m.content.toLowerCase().startsWith('.time')){
+          msg.channel.sendMessage(`Time in track: ${Math.floor(dispatcher.time / 60000)}:${Math.floor((dispatcher.time % 60000)/1000) <10 ? '0'+Math.floor((dispatcher.time % 60000)/1000) : Math.floor((dispatcher.time % 60000)/1000)}`);
+        }
+      });
+      dispatcher.on('end', () => {
+        collector.stop();
+        queue[msg.guild.id].songs.shift();
+        play(queue[msg.guild.id].songs[0]);
+      });
+      dispatcher.on('error', (err) => {
+        return msg.channel.sendMessage('Error: ' + err).then(() => {
+          collector.stop();
+          queue[msg.guild.id].songs.shift();
+          play(queue[msg.guild.id].songs[0]);
+        });
+      });
+    })(queue[msg.guild.id].songs[0]);
   },
-	'join': (msg) => {
-		return new Promise((resolve, reject) => {
-			const voiceChannel = msg.member.voiceChannel;
-			if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('I couldn\'t connect to your voice channel...');
-			voiceChannel.join().then(connection => resolve(connection)).catch(err => reject(err));
-		});
-	},
   'quality': (msg) => {
     var param = msg.content.split(' ')[1];
     if (param >= 1 && param <= 5 && (param%1)===0) {
@@ -151,9 +154,9 @@ const commands = {
       msg.channel.sendMessage(`Music quality can only be set to a whole number between 1 and 5 inclusive.`)
     }
   },
-	'add': (msg) => {
-		let url = msg.content.split(' ')[1];
-		if (url == '' || url === undefined) return msg.channel.sendMessage(`You must add a url, or youtube video id after .add!`);
+  'add': (msg) => {
+    let url = msg.content.split(' ')[1];
+    if (url == '' || url === undefined) return msg.channel.sendMessage(`You must add a url, or youtube video id after .add!`);
     try {
       yt.getInfo(url, (err, info) => {
         if (err) {return;}
@@ -164,15 +167,15 @@ const commands = {
     } catch (e) {
       msg.channel.sendMessage('Invalid YouTube Link: `' + url + '`').then(sent => {sent.delete(3000); msg.delete(3000);});
     }
-	},
-	'queue': (msg) => {
-		if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with .add!`);
-		let tosend = [];
-		queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
-		msg.channel.sendMessage(`__**${msg.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n${tosend.length==0?'':'\`\`\`'}${tosend.slice(0,15).join('\n')}${tosend.length==0?'':'\`\`\`'}`).then(() => {msg.delete();});
-	},
-	'help': (msg) => {
-		let tosend = ['```',
+  },
+  'queue': (msg) => {
+    if (queue[msg.guild.id] === undefined) return msg.channel.sendMessage(`Add some songs to the queue first with .add!`);
+    let tosend = [];
+    queue[msg.guild.id].songs.forEach((song, i) => { tosend.push(`${i+1}. ${song.title} - Requested by: ${song.requester}`);});
+    msg.channel.sendMessage(`__**${msg.guild.name}'s Music Queue:**__ Currently **${tosend.length}** songs queued ${(tosend.length > 15 ? '*[Only next 15 shown]*' : '')}\n${tosend.length==0?'':'\`\`\`'}${tosend.slice(0,15).join('\n')}${tosend.length==0?'':'\`\`\`'}`).then(() => {msg.delete();});
+  },
+  'help': (msg) => {
+    let tosend = ['```',
     '.iam <role>: Gives you a role. If you have any other roles that the bot possesses, it will attempt to remove the others for you.',
     '.clear: Clears all the roles the bot can take off you.', '.join : The bot will join the voice channel of the message\'s sender.',
     '.leave : The bot will leave all voice channels on the server.',
@@ -189,15 +192,22 @@ const commands = {
     '.volume+(+++) : Increases the volume by 2%/+.',
     '.volume-(---) : Decreases the volume by 2%/-.',
     '```'];
-		msg.channel.sendMessage(tosend.join('\n')).then(() => {msg.delete();});
-	},
+    msg.channel.sendMessage(tosend.join('\n')).then(() => {msg.delete();});
+  },
 };
 
-bot.on('message', msg => { //Make @ commands for join and leave
-	if (!msg.content.startsWith('.')) return;
-  //if (!msg.content.startsWith('.') && !(msg.content.startsWith('@') && msg.member.permissions.hasPermission("ADMINISTRATOR"))) return;
+bot.on('message', msg => {
+  if (!msg.content.startsWith('.')) return;
+  if (msg.content.startsWith('.!') && msg.member.permissions.hasPermission("ADMINISTRATOR")) {
+    try {
+      if (admCommands.hasOwnProperty(msg.content.toLowerCase().slice(2).split(' ')[0])) admCommands[msg.content.toLowerCase().slice(2%).split(' ')[0]](msg);
+    } catch (e) {
+      console.err(`\n-------\n${e}\n-------\n`);
+      errors = errors + e + '\n-------\n';
+    }
+  }
   try {
-  	if (commands.hasOwnProperty(msg.content.toLowerCase().slice(1).split(' ')[0])) commands[msg.content.toLowerCase().slice(1).split(' ')[0]](msg);
+    if (commands.hasOwnProperty(msg.content.toLowerCase().slice(1).split(' ')[0])) commands[msg.content.toLowerCase().slice(1).split(' ')[0]](msg);
   } catch (e) {
     console.err(`\n-------\n${e}\n-------\n`);
     errors = errors + e + '\n-------\n';
@@ -210,31 +220,3 @@ bot.login(token);
    response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end(errors + '\n');
 }).listen(process.env.PORT || 5000);
-
-
-//var request = require('request');
-/*
-request({
-  url: 'https://discordapp.com/api/oauth2/token',
-  method: 'POST',
-  auth: {
-    user: '274713249499447298',
-    pass: '95Wga4537Y8NEY83azjJwXxvzLidnyzH'
-  },
-  form: {
-    'grant_type': 'client_credentials'
-  }
-}, function(err, res) {
-  var json = JSON.parse(res.body);
-  console.log("Access Token:", json.access_token);
-   
-  request({
-    url: 'https://discordapp.com/api/oauth2/authorize?client-id=274713249499447298',
-    auth: {
-      'bearer': json.access_token
-    }
-  }, function(err, res) {
-    console.log(res.body);
-  });
-});
-*/
